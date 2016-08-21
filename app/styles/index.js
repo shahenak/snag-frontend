da82336eeac9a8a959a57155a6b000c5a26b885e
Carol Yao <c.y-@hotmail.com>
2016-08-21 12:05:02 -0400

    styled the divs

--- app/styles/index.js
+++ app/styles/index.js
@@ -4,17 +4,25 @@ var styles = {
   },
   container: {
     'backgroundColor':'white',
-    'margin':'auto',
+    'margin':'24px auto',
+    'padding':'16px 0',
+    'width':'60%',
     'border':'0.3px solid lightgray'
   },
   username: {
-
+    'font-size': '20px',
+    'color': 'rgba(0,0,0,0.7)',
+    'margin-bottom': '8px'
   },
   message: {
+    'font-size': '16px',
+    'color': 'rgba(0,0,0,0.7)',
+    'margin-bottom': '8px'
 
   },
   date: {
-
+    'font-size': '14px',
+    'color': 'rgba(0,0,0,0.5)'
   }
 };
 
